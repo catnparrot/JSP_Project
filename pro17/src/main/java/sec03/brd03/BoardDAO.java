c322640e2e2276396c17b691bbf63f9e8e5c46b2
catnparrot <idiotpi3rrot@gmail.com>
2023-06-11 13:20:35 +0900

    게시판구현3: 업로드 이미지파일 저장 위치 생성

---
+++ pro17/src/main/java/sec03/brd03/BoardDAO.java
@@ -0,0 +1,111 @@
+package sec03.brd03;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class BoardDAO {
+	private DataSource dFact;
+	Connection conn;
+	PreparedStatement pst;
+	
+	public BoardDAO() {
+		try {
+			Context ctx = new InitialContext();
+			Context envContext = (Context) ctx.lookup("java:/comp/env");
+			dFact = (DataSource) envContext.lookup("jdbc/oracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<ArticleVO> selectAllArticles(){
+		List<ArticleVO> articlesList = new ArrayList();
+		try {
+			conn=dFact.getConnection();
+			String sql = "SELECT LEVEL, articleNo, parentNO, title, content, id, writeDate"
+					+ " FROM t_board"
+					+ " START WITH parentNO=0"
+					+ " CONNECT BY PRIOR articleNO=parentNO"
+					+ " ORDER SIBLINGS BY articleNO DESC";
+			System.out.println();
+			pst = conn.prepareStatement(sql);
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				int level = rs.getInt("level");
+				int articleNO = rs.getInt("articleNO");
+				int parentNO = rs.getInt("parentNO");
+				String title = rs.getString("title");
+				String content = rs.getString("content");
+				String id = rs.getString("id");
+				Date writeDate = rs.getDate("writeDate");
+				ArticleVO article = new ArticleVO();
+				article.setLevel(level);
+				article.setArticleNO(articleNO);
+				article.setParentNO(parentNO);
+				article.setTitle(title);
+				article.setContent(content);
+				article.setId(id);
+				article.setWriteDate(writeDate);
+				articlesList.add(article);
+			}
+			rs.close();
+			pst.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return articlesList;
+	}
+	
+	private int getNewArticleNO() {
+		try {
+			conn = dFact.getConnection();
+			String sql = "SELECT max(articleNO) FROM t_board";
+			System.out.println(sql);
+			pst = conn.prepareStatement(sql);
+			ResultSet rs = pst.executeQuery();
+			if(rs.next())
+				return(rs.getInt(1) + 1);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int insertNewArticle(ArticleVO article) {
+		int articleNO = getNewArticleNO();
+		try {
+			conn=dFact.getConnection();
+			int parentNO = article.getParentNO();
+			String title = article.getTitle();
+			String content = article.getContent();
+			String id = article.getId();
+			String imageFileName = article.getImageFileName();
+			String sql = "INSERT INTO t_board (articleNO, parentNO, title, content, imageFileName, id)"
+					+ " VALUES (?, ?, ?, ?, ?, ?)";
+			System.out.println(sql);
+			pst = conn.prepareStatement(sql);
+			pst.setInt(1, articleNO);
+			pst.setInt(2, parentNO);
+			pst.setString(3, title);
+			pst.setString(4, content);
+			pst.setString(5, imageFileName);
+			pst.setString(6, id);
+			pst.executeUpdate();
+			pst.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return articleNO;
+	}
+	
+}
